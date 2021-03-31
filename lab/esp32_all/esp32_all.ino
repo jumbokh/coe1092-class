97223befa25a2925a583db1de407873f61f16224
jumbokh <jumbokh@gmail.com>
2021-03-31 22:44:48 +0800

    add lab

---
+++ lab/esp32_all/esp32_all.ino
@@ -0,0 +1,53 @@
+/*
+ * Generated using BlocklyDuino:
+ *
+ * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
+ *
+ * Date: Wed, 31 Mar 2021 14:42:59 GMT
+ */
+
+/*
+
+ * 部份程式碼由吉哥積木產生
+ * https://sites.google.com/jes.mlc.edu.tw/ljj/linkit7697
+
+*/
+
+#include <Wire.h>
+
+int i;
+
+byte myRead=0;
+byte myLEDs[]={16,8,4,2,1,128,64,32};
+byte allLEDs=255;
+byte ledAddr=0x20;
+
+void lightOn(byte myLED,byte LED_on){
+  byte myTempByte=0;
+  Wire.requestFrom((int)ledAddr, 1);
+  myRead=Wire.read();
+  Wire.beginTransmission((int)ledAddr);
+  if (LED_on==1)
+    myTempByte=(myRead & ~myLEDs[myLED]);
+  else if (LED_on==0)
+    myTempByte=(myRead | myLEDs[myLED]);
+  Wire.write(myTempByte);
+  Wire.endTransmission();
+}
+
+void setup()
+{
+
+  Wire.begin(26,27);
+}
+
+
+void loop()
+{
+  for (i = 0; i <= 4; i++) {
+    lightOn(i,HIGH);
+    delay(100);
+    lightOn(i,LOW);
+    delay(100);
+  }
+}
